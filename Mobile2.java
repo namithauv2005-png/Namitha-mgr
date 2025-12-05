56017ff7d345253781917d386eb46be5caf3511a
namithauv2005-png <namithauv2005@gmail.com>
2025-12-04 22:46:40 -0800

    Add files via upload

---
+++ Mobile2.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class Mobile2 {
+public void takePhoto() {
+	System.out.println("take photo");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Mobile obj=new Mobile();
+obj.takePhoto();
+	}
+}
