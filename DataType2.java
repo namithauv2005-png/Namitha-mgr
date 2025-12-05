85f9ffbac44d4aaf45bd2198c806733a07c4d75c
namithauv2005-png <namithauv2005@gmail.com>
2025-12-04 21:03:17 -0800

    Add files via upload

---
+++ DataType2.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class DataType2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int ageoftheperson=20;
+char logo='t';
+String name="Namitha";
+System.out.println(ageoftheperson);
+System.out.println(logo);
+System.out.println(name);
+	}
+
+}
