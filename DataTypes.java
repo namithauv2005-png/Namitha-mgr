5a9750860a90bc6cd3a7c463039a49a833f1ebc7
namithauv2005-png <namithauv2005@gmail.com>
2025-12-04 21:01:54 -0800

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=5;
+int b=10; 
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+
+int c=2;
+int d=8;
+System.out.println(c==d);
+System.out.println(c>d);
+System.out.println(c<d);
+	}
+
+}
