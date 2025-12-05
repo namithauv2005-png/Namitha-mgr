d51394c6f6b6e4807140b7563b40355bbe72afe8
namithauv2005-png <namithauv2005@gmail.com>
2025-12-04 21:04:13 -0800

    Add files via upload

---
+++ LoopWithIteration.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class LoopWithIteration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=1;i<=10;i++) {
+	System.out.println(i);
+}
+	}
+
+}
